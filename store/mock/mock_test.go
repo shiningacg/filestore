aa645da7a70db38e104a8ebb55a79b5c1ec88c66
shlande <shlandeme@gmail.com>
2020-07-25 22:05:45 +0800

    mock仓库添加提示输出

--- store/mock/mock_test.go
+++ store/mock/mock_test.go
@@ -9,16 +9,35 @@ import (
 	"time"
 )
 
-func TestApp(t *testing.T) {
-	store := &Store{}
-	g := gateway.NewGateway(":8888", store, log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile))
+func MewStore() *Store {
+	g := gateway.NewGateway(":8888", log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile))
 	go func() {
 		for {
 			fmt.Println(g.BandWidth())
 			time.Sleep(time.Second * 10)
 		}
 	}()
-	store.g = g
+	store := &Store{g: g}
+	g.SetStore(store)
+	go func() {
+		err := g.Run()
+		if err != nil {
+			panic(err)
+		}
+	}()
+	return store
+}
+
+func TestApp(t *testing.T) {
+	g := gateway.NewGateway(":8888", log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile))
+	go func() {
+		for {
+			fmt.Println(g.BandWidth())
+			time.Sleep(time.Second * 10)
+		}
+	}()
+	store := &Store{g: g}
+	g.SetStore(store)
 	err := g.Run()
 	if err != nil {
 		panic(err)
